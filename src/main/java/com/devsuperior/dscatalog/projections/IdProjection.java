ba977385f6f18511202156a03d699e42b788f4ce
Diego Alves <djalves424@gmail.com>
2024-02-02 21:44:54 -0300

    Created projections interfaces

---
+++ src/main/java/com/devsuperior/dscatalog/projections/IdProjection.java
@@ -0,0 +1,6 @@
+package com.devsuperior.dscatalog.projections;
+
+public interface IdProjection<E> {
+
+    E getId();
+}
---
+++ src/main/java/com/devsuperior/dscatalog/projections/ProductProjection.java
@@ -0,0 +1,6 @@
+package com.devsuperior.dscatalog.projections;
+
+public interface ProductProjection extends IdProjection<Long> {
+
+    String getName();
+}
