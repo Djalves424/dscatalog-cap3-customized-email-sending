47424d8d9a81329d32858117a4080e724c5e6c75
Diego Alves <djalves424@gmail.com>
2024-02-06 16:15:14 -0300

    Generated password recovery

---
+++ src/main/java/com/devsuperior/dscatalog/entities/PasswordRecover.java
@@ -0,0 +1,67 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_password_recover")
+public class PasswordRecover {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String token;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private Instant expiration;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Instant getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Instant expiration) {
+        this.expiration = expiration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PasswordRecover that = (PasswordRecover) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
---
+++ src/main/java/com/devsuperior/dscatalog/repositories/PasswordRecoverRepository.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dscatalog.repositories;
+
+import com.devsuperior.dscatalog.entities.PasswordRecover;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+
+@Repository
+public interface PasswordRecoverRepository extends JpaRepository<PasswordRecover, Long> {
+
+    @Query("SELECT obj FROM PasswordRecover obj WHERE obj.token = :token AND obj.expiration > :now")
+    List<PasswordRecover> searchValidTokens(String token, Instant now);
+}
